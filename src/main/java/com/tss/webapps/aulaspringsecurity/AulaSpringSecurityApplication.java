8ef4926f38e359b92da20b3fd5ffd27a32b6d4ca
thiago-santanna <uzmaki_thiago@outlook.com>
2024-04-08 16:25:11 -0300

    Iniciando o projeto

---
+++ src/main/java/com/tss/webapps/aulaspringsecurity/AulaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tss.webapps.aulaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tss/webapps/aulaspringsecurity/AulaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.webapps.aulaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
